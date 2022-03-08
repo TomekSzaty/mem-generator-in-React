a02fcc07f687c9b68f60beb270c3df5ebf329394
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2022-03-08 18:45:55 +0100

    created header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+    return (
+        
+        <header>
+            <img src="./troll-face.png" />
+        <h1>Meme Generator</h1>
+        <h4>React Project</h4>
+        </header>
+    );
+}
