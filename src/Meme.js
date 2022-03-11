173954ecb6047704eb8832469caf1883a4fde59a
TomekSzaty <75204970+TomekSzaty@users.noreply.github.com>
2022-03-11 08:55:24 +0100

    added first "react-state"

--- src/Meme.js
+++ src/Meme.js
@@ -1,8 +1,21 @@
+import React from "react";
+import memesData from "./memesData";
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        // console.log(randomNumber);        
+        
+        setMemeImage(memesArray[randomNumber].url);
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                 className="form--input" 
                 placeholder="Top text"
@@ -13,10 +26,14 @@ export default function Meme() {
                 placeholder="Bottom text" 
                 type="text" />
 
-                <button className="form--button">
-                    Get a new image 
+                <button 
+                className="form--button" 
+                onClick={getMemeImage}
+                >
+                    Get a new image 🖼 
                 </button>
-            </form>
+            </div>
+            <img src={memeImage} className="meme--image" />
         </main>
     );
 }
